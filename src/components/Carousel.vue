04cbfe9ea68f81e01868835d21e5cfd2b8453981
dfefegrgr <jiade@jiade.com>
2022-05-08 16:28:39 +0800

    1

---
+++ src/components/Carousel.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide"/>
+    <!--    导航-->
+    <div v-if="navEnable" class="navigate">
+      <!--      切换页面按钮-->
+      <div class="toggle-page left">
+        <div @click="prevSlide" class="lefticon"></div>
+      </div>
+      <div class="toggle-page right">
+        <div @click="nextSlide" class="righticon"></div>
+      </div>
+
+    </div>
+    <!--    pagination 分页-->
+    <div v-if="paginationEnable" class="pagination">
+      <!-- 判断当(index===标识)时添加active-class-->
+      <span
+          @click="goToSlide(index)"
+          v-for="(slide,index) in getSlideCount"
+            :key="index"
+            :class="{active:index+1===currentSlide}"
+      >
+
+      </span>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import {ref,onMounted} from  "vue"
+export default {
+  props:["startAutoPlay","timeout","navigation","pagination"],
+  setup(props){
+    //当前图片
+    const currentSlide = ref(1);
+    const getSlideCount = ref(null)
+    const autoPlayEnable = ref(props.startAutoPlay === undefined ? true : props.startAutoPlay)
+    const timeoutDuration = ref(props.timeout === undefined ? true : props.timeout)
+    const paginationEnable = ref(props.pagination === undefined ? true : props.pagination)
+    const navEnable = ref(props.navigation === undefined ? true : props.navigation)
+    //下一个
+    //下一个
+
+    const nextSlide=()=>{
+      if (currentSlide.value===getSlideCount.value){
+        currentSlide.value=1;
+        return;
+      }
+      console.log(currentSlide.value)
+      currentSlide.value+=1;
+
+    }
+    //上一个
+    const prevSlide=()=>{
+      if (currentSlide.value===1){
+        currentSlide.value =1;
+        return;
+      }
+      currentSlide.value-=1;
+    }
+    //翻页
+    const goToSlide =(index)=>{
+      currentSlide.value=index+1
+    }
+
+    //自动播放
+    const autoPlay =()=>{
+      setInterval(()=>{
+        nextSlide()
+      },timeoutDuration.value)
+    }
+    //执行自动播放
+    if(autoPlayEnable){
+      autoPlay()
+    }
+    //onMounted(钩子），第一次渲染时做什么
+    onMounted(()=>{
+      getSlideCount.value= document.querySelectorAll('.slide').length;
+      console.log(getSlideCount.value)
+    })
+    return{currentSlide,prevSlide,nextSlide,getSlideCount,
+      goToSlide,paginationEnable,navEnable};
+  },
+
+};
+
+</script>
+
+<style>
+.navigate{
+  padding: 0 16px;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.toggle-page{
+  display: flex;
+  /*flex:1 只拉伸指定的弹性项目*/
+  flex: 1;
+}
+.right{
+  justify-content: flex-end;
+}
+.righticon{
+    width: 0;
+    height: 0;
+    border: 20px solid ;
+    border-color:  transparent transparent transparent mediumaquamarine;
+}
+.lefticon{
+    width: 0;
+    height: 0;
+    border: 20px solid;
+    border-color:  transparent mediumaquamarine transparent transparent ;
+}
+.pagination{
+  position: absolute;
+  bottom: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 30px;}
+
+ span {
+  /*设置鼠标指针位于元素上时显示的鼠标光标类型。*/
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: aquamarine;
+  box-shadow: 0 1px 3px 0 rgba(0,0,0,0.1),0 1px 2px 0 rgba(0,0,0,0.06);
+}
+.active {
+  background-color: white;
+}
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <carousel
+        :navigation="true"
+        :pagination="true"
+        :start-auto-play="false"
+        :timeout="100000"
+        class="carousel"
+        v-slot="{ currentSlide }">
+      <!-- 循环-->
+      <slide v-for="(slide,index) in carouselSlides" :key="index">
+        <!--当 v-show_“条件”为 true 时显示元素-->
+        <!-- <div v-show="checkflg">チェックが入るとこの要素が表示されます</div>-->
+        <!--<div v-show="!checkflg">チェックが入っていませんので表示されません</div>-->
+        <div v-show="currentSlide === index +1" class="slide-info">
+          <!-- 使用${}进行拼接-->
+          <img :src="require(`../assets/${slide}.jpg`)" alt=""/>
+        </div>
+      </slide>
+    </carousel>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import carousel from "@/components/Carousel";
+import slide from "@/components/Slide";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {carousel,slide},
+  setup(){
+    //创建一个图片的数组
+    const carouselSlides=["bg-1","bg-2","bg-3"];
+    return{ carouselSlides };
+
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+
+
+.carousel{
+  //vh：视口（屏幕大小）高度的百分比。 100vh 表示与屏幕高度相同 （100%）
+  //vw：视口宽度的百分比
+  max-height: 100vh;
+  height: 100vh;
+  overflow: hidden;
+
+
+  .slide-info {
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    //left: 300px;
+    //让所有图片全部置顶
+    //width: 80%;
+    //height: 800px;
+
+
+    img{
+      width: 530px;
+      //object-fit 属性可接受如下值：
+      //fill - 默认值。调整替换后的内容大小，以填充元素的内容框。如有必要，将拉伸或挤压物体以适应该对象。
+      //contain - 缩放替换后的内容以保持其纵横比，同时将其放入元素的内容框。
+      //cover - 调整替换内容的大小，以在填充元素的整个内容框时保持其长宽比。该对象将被裁剪以适应。
+      object-fit: fill;
+      //transform: translate(0,-60px);
+    }
+  }
+}
+</style>
+
